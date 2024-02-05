0c2949d9b1ddc9107cf53fc2e646d81b9ff2d07f
Mariano Luis Arias <mariano.arias@neoris.com>
2024-02-05 16:02:37 -0300

    fix cors error

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,24 @@
 using API.Data;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+
+var Origins = "_origins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: Origins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200",
+                                              "https://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
@@ -12,23 +27,31 @@ builder.Services.AddDbContext<DataContext>(opt =>
 }
 );
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+
+//}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(Origins);
+
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
