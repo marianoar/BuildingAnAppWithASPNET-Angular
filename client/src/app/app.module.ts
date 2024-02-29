5d8c749fc34ec4550fe0f9b16d97556f6f36e7bd
Mariano Luis Arias <kurukura.dev@gmail.com>
2024-02-29 11:10:14 -0300

    added Bootswatch and shared module, end section 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './modules/shared.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -32,11 +31,8 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
     AppRoutingModule,
-    ToastrModule.forRoot({
-      // positionClass: 'toast-bottom-right', seteo la posicion
-    }),
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
