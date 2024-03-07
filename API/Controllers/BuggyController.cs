1038c93fec1049b9a8a440c8e52c36930b92aa77
Mariano Luis Arias <kurukura.dev@gmail.com>
2024-03-07 10:56:29 -0300

    Exception handling middleware

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,57 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseAPIController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if (thing == null)
+                return NotFound();
+            return thing;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            //elimina los try catch para dejar las exceptions ser manejadas por a middleware
+            //try
+            //{
+
+                var thing = _context.Users.Find(-1);
+                var thingToReturn = thing.ToString();
+                return thingToReturn;
+            //}
+            //catch (Exception ex)
+            //{
+            //    return StatusCode(500, "Error: "+ex.Message);
+            //}
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+
+    }
+}
---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Errors
+{
+    //this is going to contain the response of what we going to send back to the client
+    public class ApiException
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public string Details { get; set; }
+
+        public ApiException(int statusCode, string message, string details)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+    }
+}
