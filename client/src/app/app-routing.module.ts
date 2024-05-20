64281121997f059cfe263dde50648b00b601f518
Mariano Arias <kurukura.dev@gmail.com>
2024-05-20 11:20:15 -0300

    member edit c and fix styles and fa

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { authGuard } from './guards/auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,6 +25,7 @@ const routes: Routes = [
         canActivate: [authGuard],
       },
       { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'member/edit', component: MemberEditComponent},
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
+    MemberEditComponent,
   ],
   imports: [
     BrowserModule,
